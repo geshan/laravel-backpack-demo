df3aaba369ec4483454b273608e9d0df6cb53b2d
Geshan Manandhar <geshan@simplywallst.com>
2022-10-23 07:43:30 +1100

    Basic laravel app with backpack

---
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -0,0 +1,4 @@
+{{-- This file is used to store sidebar items, inside the Backpack admin panel --}}
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
+
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('user') }}"><i class="nav-icon la la-question"></i> Users</a></li>
